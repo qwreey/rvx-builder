6f2f5ec675171c2500c8beb92973d0695e4850b5
inotia00 <ars258000@gmail.com>
2023-07-11 23:36:04 +0900

    chore: bump version to v3.9.4

--- wsEvents/checkForUpdates.js
+++ wsEvents/checkForUpdates.js
@@ -1,6 +1,6 @@
 const { getDownloadLink } = require('../utils/FileDownloader.js');
 
-const currentVersion = 'v3.9.3';
+const currentVersion = 'v3.9.4';
 
 /**
  * @param {import('ws').WebSocket} ws

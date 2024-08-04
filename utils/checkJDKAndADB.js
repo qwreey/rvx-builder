f8f6678d08993131f500fcfcf0645bce754234ea
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-08-04 11:17:16 +0900

    feat: update hard coded JDK version

--- utils/checkJDKAndADB.js
+++ utils/checkJDKAndADB.js
@@ -11,7 +11,7 @@ function getConsts(name) {
     case 'jreDir':
       return joinPath(
         global.revancedDir,
-        `zulu17.50.19-ca-jre17.0.11-${getConsts('platform')}_${getConsts(
+        `zulu17.52.17-ca-jre17.0.12-${getConsts('platform')}_${getConsts(
           'arch'
         )}`
       );
@@ -61,7 +61,7 @@ async function downloadJDK(ws) {
     const output = joinPath(global.revancedDir, 'JRE.tar.gz');
 
     await dloadFromURL(
-      `https://cdn.azul.com/zulu/bin/zulu17.50.19-ca-jre17.0.11-${getConsts(
+      `https://cdn.azul.com/zulu/bin/zulu17.52.17-ca-jre17.0.12-${getConsts(
         'platform'
       )}_${getConsts('arch')}.tar.gz`,
       output,
